028852e0b17a0c06e7b19dc191279fccec929cbc
Semra Genç <semrademir292@gmail.com>
2025-06-24 15:05:08 +0300

    IlearingReact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './components/Header';
+import Login from './components/Login';
+const isLoggedIn= false;
+const lookingForJob= true;
+const name = "Semra "; //Sabiti component içinde nasıl render edeceğim? {name}
+const surname = "Genç";
+const cityOfResidence = "Balıkesir";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className='test'> 
+    {
+      //isLoggedIn ? "Giriş Başarılı " + name  + surname : <a href='#/'>Giriş Yapınız</a>
+      // ? short if oluyor
+      // : short else oluyor 
+    }
+    <br></br>
+    {//lookingforjob doğru ise 
+      lookingForJob && <div>I'm looking for a job {name + surname}</div>
+    }
+    { //lookingforjob yanlış ise
+      !lookingForJob && <a href='#/'>My new workplace</a>
+    }
+    <br></br>
+    {isLoggedIn ? <div>Hoş Geldin {name + surname}</div> : <Login/>}
+    <br></br>
+    {cityOfResidence}
+    <br></br>
+    {name} {surname}
+    <br></br>
+    Welcome
+    <Header/>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+    <div>Goodbye</div>
+  </div>
   );
 }
 
 export default App;
+
+//class yerine ClassName kullanılıyor. ZORUNLU 
+//class = className
+//label for = label htmlfor
---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Login() {
+    return (
+        <div>Login
+            <input placeholder="email"/>
+        </div>
+    );
+}
+export default Login;
