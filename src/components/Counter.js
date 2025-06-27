75568ff0bb2e1f2539747518725cabe4029e9bba
Semra Genç <semrademir292@gmail.com>
2025-06-27 16:52:15 +0300

    Project is finish.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,12 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 //State in kullanımı, değişken ismi, fonksiyon ismi, başlangıç değeri
 
 function Counter() {
     const [count, setCount] = useState(0);
     const [amount, setAmount] = useState(1);
+    useEffect(() => {
+      console.log("Bir state değişti.")
+    })
   return (
     <div>
         <h1>{count}</h1>
